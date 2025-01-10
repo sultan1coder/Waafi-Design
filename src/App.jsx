39790057cf97d83d19397f3849f601ba5338380b
ashraf <sadarsign@gmail.com>
2025-01-10 23:14:26 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./styles/style.css";
 import "./styles/hero2.css"
+import "./styles/hero3.css"
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Hero2 from "./components/hero2"
+import Hero3 from "./components/hero3";
 
 function App() {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
@@ -17,6 +19,7 @@ function App() {
       <Header toggleMenu={toggleMenu} />
       <Hero isMenuVisible={isMenuVisible} />
       <Hero2 />
+      <Hero3 />
     </div>
   );
 }
---
+++ src/components/hero3.jsx
@@ -0,0 +1,65 @@
+function Hero3() {
+    return (
+        <>
+        <div className="part-1">
+            <div className="text">
+            <h1>Meet Our Team</h1>
+            <p>We have a team of experienced professionals who are <br /> dedicated to making your experience with WAAFI as <br /> seamless as possible.</p>
+            </div>
+            <div className="btn">
+                <div className="ones">
+                    <img src="https://page.waafi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fchairman.d06c6fcc.png&w=256&q=75" alt="" />
+                    <h1>Ali Abdallah Hettam</h1>
+                    <h2>Chairman</h2>
+                    <p>39+ years in logistics, sea transport, <br /> and business development. Fluent <br /> in English, French, and Arabic.</p>
+                </div>
+                <div className="ones">
+                    <img src="https://page.waafi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FCEO.f3d1eee6.png&w=256&q=75" alt="" />
+                    <h1>Abdoulkadir Mohamed Djama</h1>
+                    <h2>CEO</h2>
+                    <p>Born in 1986, Master’s in Public <br /> Administration. CEO of WAAFI SAS, <br /> former Djibouti Health Rep in India. <br /> Banking experience. Fluent in <br /> English and French.</p>
+                </div>
+                <div className="ones">
+                    <img src="https://page.waafi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FCTO.a4dfb3fb.jpg&w=256&q=75" alt="" />
+                    <h1>Abdirizak Muse</h1>
+                    <h2>CTO</h2>
+                    <p>10+ years in mobile financial <br /> services. Led projects from <br /> architecture to execution. Expert <br /> in PCI DSS, PA DSS, and security <br /> threats.</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="part-2">
+            <h1>About Us</h1>
+            <div className="text">
+                <div className="p1">
+                    <p>Our Company</p>
+                </div>
+                <div>
+                    <p>Coorporate Governance</p>
+                </div>
+                <div>
+                    <p>Application Process</p>
+                </div>
+            </div>
+
+            <div className="text-2">
+                <h2>Waafi SAS</h2>
+                <p>At Waafi SAS, we want you to make the most of your time, simplifying your daily needs with our groundbreaking All-In-One <br /> lifestyle e-wallet app. Waafi is a global lifestyle e-wallet app, created with the idea to be more than a regular mobile <br /> payment app, we designed it to become your daily companion, at the tip of your fingers with 24/7 support and access to <br /> our services such as; Mobile Money, Communication, Entertainment and Payments. Want to book a flight or pay your bills <br /> on the go? Making payments through the Waafi app is a few clicks away – as easy as one, two, three! We want you to be <br /> part of our growing community, join Waafi today.</p>
+
+                <h2>Our Mission</h2>
+                <p>Our mission at Waafi is to provide everybody a secure and reliable cashless mobile payment platform with outstanding <br /> user experience that acts like a companion that simplifies your life.</p>
+                
+                <h2>Our Vision</h2>
+                <p>
+                To become the consumer&qoute;s first choice as a service that can withstand their daily online payment needs – a true <br /> companion. To make the Waafi brand a household name, synonymous with reliability, efficiency and security</p>
+
+                <h2>Our Values</h2>
+                <p>From the very start, we shaped our organization around the same values and culture Waafi was founded on. We believe, <br /> creating an environment for openness and cooperation for our employees within the company leads to more innovation <br /> and growth which in turn produces a better service for our customers</p>
+            </div>
+        </div>
+        </>
+    );
+}
+
+
+export default Hero3;
